a3d1d0c81ee0765c43109db16b83f4f213e123f9
Фёдор Воропаев <voropaevfedor2005@mail.ru>
2022-06-22 16:45:37 +0500

    посмотрел практику сделал бивиса и батхеда и им дом

--- first_course_python_developer/lesson_007/python_snippets/08_practice.py
+++ first_course_python_developer/lesson_007/python_snippets/08_practice.py
@@ -8,69 +8,91 @@ from random import randint
 # У человека есть степень сытости, немного еды и денег.
 # Если сытость < 0 единиц, человек умирает.
 # Человеку надо прожить 365 дней.
-from termcolor import cprint
+from termcolor import cprint, colored
 
 
 class Man:
 
     def __init__(self, name):
         self.fullness = 50
-        self.food = 50
-        self.money = 0
         self.name = name.title()
+        self.house = None
 
     def __str__(self):
-        return f'Я - {self.name}, я сыт на {self.fullness}%, у меня {self.food} еды и {self.money} денег '
+        return colored(f'Я - {self.name}, я сыт на {self.fullness}%', color='green')
 
     def eat(self):
-        if self.food >= 10:
-            print(f'{self.name} поел')
+        if self.house.food >= 10:
+            cprint(f'{self.name} поел', color='blue')
             self.fullness += 10
-            self.food -= 10
+            self.house.food -= 10
         else:
-            print(f'У {self.name} нет еды (')
+            cprint(f'У {self.name} нет еды (', color='red')
 
     def work(self):
-        print(f'{self.name} сходил на работу')
-        self.money += 50
+        cprint(f'{self.name} сходил на работу', color='blue')
+        self.house.money += 50
         self.fullness -= 10
 
-    def play_dota(self):
-        cprint(f'{self.name} поиграл в доту', color='green')
+    def watch_MTV(self):
+        cprint(f'{self.name} смотрел MTV', color='blue')
         self.fullness -= 10
+
+    def go_into_the_house(self, house):
+        self.house = house
+        self.fullness -= 10
+        cprint(f'{self.name} заехал в дом', color='cyan')
 
     def shopping(self):
-        if self.money >= 50:
-            print(f'{self.name} сходил в магазин за едой')
-            self.money -= 50
-            self.food += 50
+        if self.house.money >= 50:
+            cprint(f'{self.name} сходил в магазин за едой', color='blue')
+            self.house.money -= 50
+            self.house.food += 50
         else:
-            print(f'У {self.name} кончились деньги')
+            cprint(f'У {self.name} кончились деньги', color='red')
 
     def act(self):
         if self.fullness <= 0:
-            print(f'{self.name} умер от голода')
+            cprint(f'{self.name} умер от голода', color='red')
             return
         dice = randint(1, 6)
         if self.fullness < 20:
             self.eat()
-        elif self.food < 10:
+        elif self.house.food < 10:
             self.shopping()
-        elif self.money < 50:
+        elif self.house.money < 50:
             self.work()
         elif dice == 1:
             self.work()
         elif dice == 2:
             self.eat()
         else:
-            self.play_dota()
+            self.watch_MTV()
 
 
-vasya = Man('Вася')
-for day in range(1, 366):
-    cprint(f'============= День{day} =============', color='yellow')
-    vasya.act()
-    print(vasya)
+class House:
+    def __init__(self):
+        self.food = 10
+        self.money = 50
+
+    def __str__(self):
+        return colored(f'В доме осталось {self.food} еды и {self.money} денег', color='green')
+
+
+my_sweet_home = House()
+beavis = Man('Бивис')
+butthead = Man('Батхед')
+
+beavis.go_into_the_house(my_sweet_home)
+butthead.go_into_the_house(my_sweet_home)
+for day in range(1, 21):
+    cprint(f'============= День {day} =============', color='yellow')
+    beavis.act()
+    butthead.act()
+    print('------------- В конце дня -------------')
+    print(beavis)
+    print(butthead)
+    print(my_sweet_home)
 # Создадим двух людей, живущих в одном доме - Бивиса и Батхеда
 # Нужен класс Дом, в нем должн быть холодильник с едой и тумбочка с деньгами
 # Еда пусть хранится в холодильнике в доме, а деньги - в тумбочке.
