4b186444603a6cdea0dd2b9a27394c04661c0f52
Фёдор Воропаев <voropaevfedor2005@mail.ru>
2022-07-06 16:42:09 +0500

    прошел практику в которой прочитал файл и сделал вычисления

--- first_course_python_developer/lesson_010/python_snippets/06_practice.py
+++ first_course_python_developer/lesson_010/python_snippets/06_practice.py
@@ -12,7 +12,7 @@
 
 
 def calc(line):
-    # print(f'Read line {line}', flush=True)
+    # print(f'Working with {line}', flush=True)
     operand_1, operation, operand_2 = line.split(' ')
     operand_1 = int(operand_1)
     operand_2 = int(operand_2)
@@ -23,26 +23,25 @@ def calc(line):
     elif operation == '/':
         value = operand_1 / operand_2
     elif operation == '*':
-        value = operand_1 / operand_2
+        value = operand_1 * operand_2
     elif operation == '//':
         value = operand_1 // operand_2
     elif operation == '%':
         value = operand_1 % operand_2
     else:
-        raise ValueError('Unknown operation {operation}')
+        print(f'Unknown operation {operation}')
     return value
 
 
 total = 0
-with open('calc.txt', 'r') as ff:
-    for line in ff:
+with open('calc.txt', 'r') as file:
+    for line in file:
         line = line[:-1]
         try:
             total += calc(line)
         except ValueError as exc:
             if 'unpack' in exc.args[0]:
-                print(f'Не хватает операндов {exc} в строке {line}')
+                print(f'Не хватает операндов {exc} в строке:{line}')
             else:
-                print(f'Не могу преобразовать к целому {exc} в строке {line}')
-
+                print(f'Не могу преобразовать к целому {exc} в строке:{line}')
 print(f'Total {total}')
