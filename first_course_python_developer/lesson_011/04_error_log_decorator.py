73004db3590e373df929b6e905338aa8d64a0475
Фёдор Воропаев <voropaevfedor2005@mail.ru>
2022-07-13 17:14:12 +0500

    сделал задание 4

--- first_course_python_developer/lesson_011/04_error_log_decorator.py
+++ first_course_python_developer/lesson_011/04_error_log_decorator.py
@@ -9,8 +9,14 @@
 
 
 def log_errors(func):
-    pass
-    # TODO здесь ваш код
+    def surrogate(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as exc:
+            with open('function_errors.log', 'a', encoding='utf8') as file:
+                file.write(f'{func.__name__} {args, kwargs} {type(exc)} {exc}\n')
+    return surrogate
 
 
 # Проверить работу на следующих функциях
@@ -52,4 +58,46 @@ perky(param=42)
 # @log_errors('function_errors.log')
 # def func():
 #     pass
+def logs_errors(filename):
+    def log_errors(func):
+        def surrogate(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                return result
+            except Exception as exc:
+                with open(filename, 'a', encoding='utf8') as file:
+                    file.write(f'{func.__name__} {args, kwargs} {type(exc)} {exc}\n')
+        return surrogate
+    return log_errors
 
+
+@logs_errors('file.txt')
+def perky(param):
+    return param / 0
+
+
+@logs_errors('file.txt')
+def check_line(line):
+    name, email, age = line.split(' ')
+    if not name.isalpha():
+        raise ValueError("it's not a name")
+    if '@' not in email or '.' not in email:
+        raise ValueError("it's not a email")
+    if not 10 <= int(age) <= 99:
+        raise ValueError('Age not in 10..99 range')
+
+
+lines = [
+    'Ярослав bxh@ya.ru 600',
+    'Земфира tslzp@mail.ru 52',
+    'Тролль nsocnzas.mail.ru 82',
+    'Джигурда wqxq@gmail.com 29',
+    'Земфира 86',
+    'Равшан wmsuuzsxi@mail.ru 35',
+]
+for line in lines:
+    try:
+        check_line(line)
+    except Exception as exc:
+        print(f'Invalid format: {exc}')
+perky(param=42)
