22d16bda37c53b8a590ac75a5fe09aa27efaf296
Фёдор Воропаев <voropaevfedor2005@mail.ru>
2022-07-13 16:50:37 +0500

    сделал задание 3

--- first_course_python_developer/lesson_011/03_log_parser.py
+++ first_course_python_developer/lesson_011/03_log_parser.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from collections import defaultdict
 
 # На основе своего кода из lesson_009/02_log_parser.py напишите итератор (или генератор)
 # котрый читает исходный файл events.txt и выдает число событий NOK за каждую минуту
@@ -14,4 +15,21 @@
 #
 # [2018-05-17 01:57] 1234
 
-# TODO здесь ваш код
+
+def log_parser(file):
+    counter = 0
+    with open(file, 'r') as f_obj:
+        logs = defaultdict(int)
+        for line in f_obj:
+            time, status = line[:-1].split('] ')
+            if status == 'NOK':
+                logs[time[1:17]] += 1
+        logs = [(x, logs[x]) for x in logs.keys()]
+    for _ in range(len(logs)):
+        yield logs[counter]
+        counter += 1
+
+
+grouped_events = log_parser('events.txt')
+for group_time, event_count in grouped_events:
+    print(f'[{group_time}] {event_count}')
